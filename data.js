bac80f26c2c682b5cb34de8a6bc28e1f68e68c83
moh-fauzi <mohf36357@gmail.com>
2026-03-05 11:03:48 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -19,19 +19,19 @@ link: "https://www.canva.com/design/DAHC41nqxsA/QQuwXTt_7gk00Gmgfialdw/view?utm_
 },
 
 {
-title: "PPT Business Patch",
+title: "PPT Business-patch",
 category: "PPT",
 images: [
-"assets/ppt/business patch/1.png",
-"assets/ppt/business patch/2.png",
-"assets/ppt/business patch/3.png",
-"assets/ppt/business patch/4.png",
-"assets/ppt/business patch/5.png",
-"assets/ppt/business patch/6.png",
-"assets/ppt/business patch/7.png",
-"assets/ppt/business patch/8.png",
-"assets/ppt/business patch/9.png",
-"assets/ppt/business patch/10.png"
+"assets/ppt/business-patch/1.png",
+"assets/ppt/business-patch/2.png",
+"assets/ppt/business-patch/3.png",
+"assets/ppt/business-patch/4.png",
+"assets/ppt/business-patch/5.png",
+"assets/ppt/business-patch/6.png",
+"assets/ppt/business-patch/7.png",
+"assets/ppt/business-patch/8.png",
+"assets/ppt/business-patch/9.png",
+"assets/ppt/business-patch/10.png"
 ],
 link: "https://www.canva.com/design/DAHDCEEzDZw/5Op0_uvHHFXhSII9FkG2Yg/view?utm_content=DAHDCEEzDZw&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink&mode=preview"
 }
