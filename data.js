b4d5c7e4e37ccaa25aa7880c8db4fa421923e29a
moh-fauzi <mohf36357@gmail.com>
2026-03-05 03:51:10 +0000

    add business template

--- data.js
+++ data.js
@@ -1,19 +1,39 @@
 const templates = [
-  {
-    title: "PPT Cyber Security",
-    category: "PPT",
-    images: [
-      "assets/ppt/cyber-security/1.png",
-      "assets/ppt/cyber-security/2.png",
-      "assets/ppt/cyber-security/3.png",
-      "assets/ppt/cyber-security/4.png",
-      "assets/ppt/cyber-security/5.png",
-      "assets/ppt/cyber-security/6.png",
-      "assets/ppt/cyber-security/7.png",
-      "assets/ppt/cyber-security/8.png",
-      "assets/ppt/cyber-security/9.png",
-      "assets/ppt/cyber-security/10.png"
-    ],
-    link: "https://www.canva.com/design/DAHC41nqxsA/QQuwXTt_7gk00Gmgfialdw/view?utm_content=DAHC41nqxsA&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink&mode=preview"
-  }
+
+{
+title: "PPT Cyber Security",
+category: "PPT",
+images: [
+"assets/ppt/cyber-security/1.png",
+"assets/ppt/cyber-security/2.png",
+"assets/ppt/cyber-security/3.png",
+"assets/ppt/cyber-security/4.png",
+"assets/ppt/cyber-security/5.png",
+"assets/ppt/cyber-security/6.png",
+"assets/ppt/cyber-security/7.png",
+"assets/ppt/cyber-security/8.png",
+"assets/ppt/cyber-security/9.png",
+"assets/ppt/cyber-security/10.png"
+],
+link: "https://www.canva.com/design/DAHC41nqxsA/QQuwXTt_7gk00Gmgfialdw/view?utm_content=DAHC41nqxsA&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink&mode=preview"
+},
+
+{
+title: "PPT Business Patch",
+category: "PPT",
+images: [
+"assets/ppt/business patch/1.png",
+"assets/ppt/business patch/2.png",
+"assets/ppt/business patch/3.png",
+"assets/ppt/business patch/4.png",
+"assets/ppt/business patch/5.png",
+"assets/ppt/business patch/6.png",
+"assets/ppt/business patch/7.png",
+"assets/ppt/business patch/8.png",
+"assets/ppt/business patch/9.png",
+"assets/ppt/business patch/10.png"
+],
+link: "https://www.canva.com/design/DAHDCEEzDZw/5Op0_uvHHFXhSII9FkG2Yg/view?utm_content=DAHDCEEzDZw&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink&mode=preview"
+}
+
 ];
